5eb6349ebf60f993a379ea739fa59e364a3ed83f
ahmed elnady <dev.elnady@gmail.com>
2019-04-18 03:24:54 +0200

    run app in index file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const bootstrap = require('./server/server')
+
+async function runApp() {
+    const app = await bootstrap(3000, 'dbHost', 'spare-api-gateway')
+    return app; 
+}
+
+(async ()=> {
+    await runApp();
+})();
+
+module.exports = { runApp }
